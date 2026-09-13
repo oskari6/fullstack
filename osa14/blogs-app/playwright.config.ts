e900cf9a3a632fef0b055817c2774bb4c70eee9f
oskari <oskarisulkakoski@gmail.com>
2026-09-13 13:38:59 +0200

    tryign to fix broken osa14 workflow

--- osa14/blogs-app/playwright.config.ts
+++ osa14/blogs-app/playwright.config.ts
@@ -17,7 +17,8 @@ export default defineConfig({
 
   use: {
     baseURL,
-    trace: "on-first-retry",
+    trace: "retain-on-failure",
+    screenshot: "only-on-failure"
   },
   projects: [
     {
